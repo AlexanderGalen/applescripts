fadad22f55be4cfff45e9a9ae42e8eb2be973223
Alexander Carr <Alexanderc93@gmail.com>
2015-01-21 14:02:18 -0800

    add postcard merge javascript. remove pesky semicolon in return statement in export name badge applescript.

---
+++ postcard merge.js
@@ -0,0 +1,47 @@
+var hummus = require('hummus');
+var fs = require('fs');
+var excel = require('excel');
+var applescript = require('applescript');
+
+var sourceDB, baseSourceFolder, baseDestinationFolder, destinationFile, pageWidth, pageHeight;
+
+pagewidth =
+sourceDB = "/Volumes/HOM/PRODUCTS/NOTE CARD CAFE/POSTCARDS/DBs/Build Postcard Print Files.xlsx";
+baseSourceFolder = "/Volumes/HOM/PRODUCTS/NOTE CARD CAFE/POSTCARDS/printfiles/";
+baseDestinationFolder = "/Volumes/HOM/PRODUCTS/NOTE CARD CAFE/POSTCARDS/merged/";
+
+// parse excel file to get data
+excel(sourceDB, function(err, data) {
+	if(err) throw err;
+
+	// big loop for each set
+	for (var i = 1; i < data.length; i=i+6) {
+		var thisRow = data[i];
+		var setName = thisRow[0];
+		var packageInsert = baseSourceFolder + thisRow[1];
+		var outputFile = baseDestinationFolder + setName + ".102page.pdf"
+
+		var pdfWriter =	hummus.createWriter(outputFile);
+		pdfWriter.appendPDFPagesFromPDF(packageInsert);
+
+		// loop for going through all the singles of the set
+		for (var j = i; j < 7; j++) {
+			thisRow = data[j];
+			var cardFront = baseSourceFolder + thisRow[2];
+			var cardBack = baseSourceFolder + thisRow[3];
+
+			// loop for adding each single 10 times
+			for (var k = 0; k < 10; k++) {
+				pdfWriter.appendPDFPagesFromPDF(cardFront);
+				pdfWriter.appendPDFPagesFromPDF(cardBack)
+			};
+
+
+		};
+
+
+	};
+
+	pdfWriter.end();
+
+});
